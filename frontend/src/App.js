d48eb6eb898b1aca826588d208ccb4d7e19d04f0
lucifer-hell <luciferhell58@gmail.com>
2024-01-24 21:47:45 +0530

    basic app setup done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import "./App.css";
+import { Button, Input, Space } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
 
 function App() {
   const [inputText, setInputText] = useState("");
   const [result, setResult] = useState("");
-  const [loading,setLoading] =useState(false);
+  const [loading, setLoading] = useState(false);
 
   const getChankyaTeaching = async () => {
     try {
-      setLoading(true)
+      setLoading(true);
       const response = await fetch("http://localhost:8000/predict_output", {
         method: "POST",
         headers: {
@@ -18,27 +20,53 @@ function App() {
       });
       const result = await response.json();
       setResult(`Chankya's Teaching: ${result["output"]}`);
-      setLoading(false)
+      setLoading(false);
     } catch (error) {
       console.error("Error fetching data:", error);
       setResult("An error occurred. Please try again.");
-      setLoading(false)
+      setLoading(false);
     }
   };
 
   return (
     <div className="App">
       <h1>Chankya Teachings</h1>
-      <label htmlFor="inputText">Enter your question:</label>
-      <input
-        type="text"
-        id="inputText"
-        placeholder="E.g., How to determine true friendship?"
-        value={inputText}
-        onChange={(e) => setInputText(e.target.value)}
-      />
-      <button disabled={loading} onClick={getChankyaTeaching}>Get Chankya's Teaching</button>
-      <div id="result">{loading?"thinking ...":result}</div>
+      <div
+        style={{
+          backgroundColor: "#343541",
+          width: "60%",
+          marginLeft: "20%",
+          marginTop: "10%",
+          marginBottom:"10%",
+          display: "flex",
+          justifyContent: "space-between",
+          border: "1px solid grey",
+          borderRadius: "1.2%",
+        }}
+      >
+        <Input
+          style={{
+            backgroundColor: "#343541",
+            boxShadow: "none",
+            color: "whitesmoke",
+            borderColor: "#343541",
+            width: "95%",
+          }}
+          id="inputText"
+          placeholder="E.g., How to determine true friendship?"
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+        />
+        <Space>
+          <Button
+            type="secondary"
+            icon={<SearchOutlined />}
+            onClick={getChankyaTeaching}
+          />
+        </Space>
+      </div>
+
+      <div id="result">{loading ? "thinking ..." : result}</div>
     </div>
   );
 }
